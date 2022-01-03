9717df778444f450bb71b3afeb342e5a79ed5124
Anthony Lapenok <96385694+LipsarJ@users.noreply.github.com>
2022-01-04 00:24:28 +0300

    Update option_sl.c
    Фикс опций профиля

--- Program/INTERFACE/option_sl.c
+++ Program/INTERFACE/option_sl.c
@@ -35,10 +35,10 @@ void PrepareDefaultOption(ref optref)
 	optref.video.grassquantity = 0;
 	optref.seadetails = 1.0;
 	//#20171223-01 Camera perspective option
-    optref.perspdetails = 0.0;
-    //#20190815-01
-    optref.persp2details = 0.0;
-    optref.raddetails = 0.0;
+	optref.perspdetails = 0.0;
+	//#20190815-01
+	optref.persp2details = 0.0;
+	optref.raddetails = 0.0;
 
 	SeaParametrs.MaxVertices = MaxVertices;
 	SeaParametrs.MaxIndices = MaxIndices;
@@ -50,12 +50,12 @@ void PrepareDefaultOption(ref optref)
 
 void GetRealOptions(ref optref)
 {
-	float ftmp1,ftmp2,ftmp3;
+	float ftmp1, ftmp2, ftmp3;
 
 	ftmp1 = stf(optref.volume.sound);
 	ftmp2 = stf(optref.volume.music);
 	ftmp3 = stf(optref.volume.dialog);
-	SendMessage(&sound,"leee", MSG_SOUND_GET_MASTER_VOLUME, &ftmp1, &ftmp2,	&ftmp3 );
+	SendMessage(&sound, "leee", MSG_SOUND_GET_MASTER_VOLUME, &ftmp1, &ftmp2, &ftmp3);
 	optref.volume.sound = ftmp1;
 	optref.volume.music = ftmp2;
 	optref.volume.dialog = ftmp3;
@@ -74,149 +74,219 @@ void GetRealOptions(ref optref)
 	optref.cameramode.follow_on = !locCameraEnableSpecialMode;
 
 	optref.video.grassquantity = iGrassQuality;
-	if( CheckAttribute(&InterfaceStates,"SeaDetails") ) {
+	if (CheckAttribute(&InterfaceStates, "SeaDetails"))
+	{
 		optref.seadetails = stf(InterfaceStates.SeaDetails);
 	}
-    //#20171223-01 Camera perspective option
-	if( CheckAttribute(&InterfaceStates,"PerspDetails") ) {
+	//#20171223-01 Camera perspective option
+	if (CheckAttribute(&InterfaceStates, "PerspDetails"))
+	{
 		optref.perspdetails = stf(InterfaceStates.PerspDetails);
 	}
 	//#20190815-01
-	if( CheckAttribute(&InterfaceStates,"Persp2Details") ) {
+	if (CheckAttribute(&InterfaceStates, "Persp2Details"))
+	{
 		optref.persp2details = stf(InterfaceStates.Persp2Details);
 	}
-	if( CheckAttribute(&InterfaceStates,"RadDetails") ) {
+	if (CheckAttribute(&InterfaceStates, "RadDetails"))
+	{
 		optref.raddetails = stf(InterfaceStates.RadDetails);
 	}
-	if( CheckAttribute(&InterfaceStates,"InvertCameras") ) {
+	if (CheckAttribute(&InterfaceStates, "InvertCameras"))
+	{
 		optref.cameramode.InvertCameras = sti(InterfaceStates.InvertCameras);
-	} else {
+	}
+	else
+	{
 		optref.cameramode.InvertCameras = false;
 	}
 
-	if( CheckAttribute(&InterfaceStates,"ShowBattleMode") ) {
+	if (CheckAttribute(&InterfaceStates, "ShowBattleMode"))
+	{
 		optref.cameramode.ShowBattleMode = sti(InterfaceStates.ShowBattleMode);
-	} else {
+	}
+	else
+	{
 		optref.cameramode.ShowBattleMode = false;
 	}
 
-	if( CheckAttribute(&InterfaceStates,"EnabledAutoSaveMode") ) {
+	if (CheckAttribute(&InterfaceStates, "EnabledAutoSaveMode"))
+	{
 		optref.cameramode.EnabledAutoSaveMode = sti(InterfaceStates.EnabledAutoSaveMode);
-	} else {
+	}
+	else
+	{
 		optref.cameramode.EnabledAutoSaveMode = true;
 	}
 
-	if( CheckAttribute(&InterfaceStates,"EnabledQuestsMarks") ) {
+	if (CheckAttribute(&InterfaceStates, "EnabledQuestsMarks"))
+	{
 		optref.cameramode.EnabledQuestsMarks = sti(InterfaceStates.EnabledQuestsMarks);
-	} else {
+	}
+	else
+	{
 		optref.cameramode.EnabledQuestsMarks = true;
 	}
 
-	if( CheckAttribute(&InterfaceStates,"EnabledFXMarks") ) {
+	if (CheckAttribute(&InterfaceStates, "EnabledFXMarks"))
+	{
 		optref.cameramode.EnabledFXMarks = sti(InterfaceStates.EnabledFXMarks);
-	} else {
+	}
+	else
+	{
 		optref.cameramode.EnabledFXMarks = true;
 	}
 
-	if( CheckAttribute(&InterfaceStates,"EnabledCMControls") ) {
+	if (CheckAttribute(&InterfaceStates, "EnabledCMControls"))
+	{
 		optref.cameramode.EnabledCMControls = sti(InterfaceStates.EnabledCMControls);
-	} else {
+	}
+	else
+	{
 		optref.cameramode.EnabledCMControls = true;
 	}
 
-	if( CheckAttribute(&InterfaceStates,"EnabledShipMarks") ) {
+	if (CheckAttribute(&InterfaceStates, "EnabledShipMarks"))
+	{
 		optref.cameramode.EnabledShipMarks = sti(InterfaceStates.EnabledShipMarks);
-	} else {
+	}
+	else
+	{
 		optref.cameramode.EnabledShipMarks = true;
 	}
 
-	if( CheckAttribute(&InterfaceStates,"SimpleSea") ) {
+	if (CheckAttribute(&InterfaceStates, "SimpleSea"))
+	{
 		optref.cameramode.SimpleSeaMode = sti(InterfaceStates.SimpleSea);
-	} else {
+	}
+	else
+	{
 		optref.cameramode.SimpleSeaMode = true;
 	}
 
-	if( CheckAttribute(&InterfaceStates,"SpyglassTextures") ) {
+	if (CheckAttribute(&InterfaceStates, "SpyglassTextures"))
+	{
 		optref.cameramode.SpyglassTextures = sti(InterfaceStates.SpyglassTextures);
-	} else {
+	}
+	else
+	{
 		optref.cameramode.SpyglassTextures = true;
 	}
 
-	if( CheckAttribute(&InterfaceStates,"HUDStyle") ) {
+	if (CheckAttribute(&InterfaceStates, "HUDStyle"))
+	{
 		optref.cameramode.HUDStyle = sti(InterfaceStates.HUDStyle);
-	} else {
+	}
+	else
+	{
 		optref.cameramode.HUDStyle = true;
 	}
 
-	if( CheckAttribute(&InterfaceStates,"VISUAL_CIRASS") ) {
+	if (CheckAttribute(&InterfaceStates, "VISUAL_CIRASS"))
+	{
 		optref.cameramode.VISUAL_CIRASS = sti(InterfaceStates.VISUAL_CIRASS);
-	} else {
+	}
+	else
+	{
 		optref.cameramode.VISUAL_CIRASS = 0;
 	}
 
-	if( CheckAttribute(&InterfaceStates,"CannonsHUD") ) {
+	if (CheckAttribute(&InterfaceStates, "CannonsHUD"))
+	{
 		optref.cameramode.CannonsHUD = sti(InterfaceStates.CannonsHUD);
-	} else {
+	}
+	else
+	{
 		optref.cameramode.CannonsHUD = true;
 	}
 
-	if( CheckAttribute(&InterfaceStates,"DeadBoxText") ) {
+	if (CheckAttribute(&InterfaceStates, "DeadBoxText"))
+	{
 		optref.cameramode.DeadBoxText = sti(InterfaceStates.DeadBoxText);
-	} else {
+	}
+	else
+	{
 		optref.cameramode.DeadBoxText = true;
 	}
 
-	if( CheckAttribute(&InterfaceStates,"AltFont") ) {
+	if (CheckAttribute(&InterfaceStates, "AltFont"))
+	{
 		optref.cameramode.AltFont = sti(InterfaceStates.AltFont);
-	} else {
+	}
+	else
+	{
 		optref.cameramode.AltFont = 0;
 	}
 
-	if( CheckAttribute(&InterfaceStates,"NoInt") ) {
+	if (CheckAttribute(&InterfaceStates, "NoInt"))
+	{
 		optref.cameramode.NoInt = sti(InterfaceStates.NoInt);
-	} else {
+	}
+	else
+	{
 		optref.cameramode.NoInt = true;
 	}
 
-	if( CheckAttribute(&InterfaceStates,"IslandLoader") ) {
+	if (CheckAttribute(&InterfaceStates, "IslandLoader"))
+	{
 		optref.cameramode.IslandLoader = sti(InterfaceStates.IslandLoader);
-	} else {
+	}
+	else
+	{
 		optref.cameramode.IslandLoader = true;
 	}
 
-	if( CheckAttribute(&InterfaceStates,"AltIntIcons") ) {
+	if (CheckAttribute(&InterfaceStates, "AltIntIcons"))
+	{
 		optref.cameramode.AltIntIcons = sti(InterfaceStates.AltIntIcons);
-	} else {
+	}
+	else
+	{
 		optref.cameramode.AltIntIcons = true;
 	}
 
 	GetControlsOptions(optref);
 
 	// mouse
-	if( CheckAttribute(&InterfaceStates,"mouse.x_sens") ) {
+	if (CheckAttribute(&InterfaceStates, "mouse.x_sens"))
+	{
 		optref.mouse.x_sensitivity = InterfaceStates.mouse.x_sens;
-	} else {
+	}
+	else
+	{
 		optref.mouse.x_sensitivity = 0.5;
 	}
-	if( CheckAttribute(&InterfaceStates,"mouse.y_sens") ) {
+	if (CheckAttribute(&InterfaceStates, "mouse.y_sens"))
+	{
 		optref.mouse.y_sensitivity = InterfaceStates.mouse.y_sens;
-	} else {
+	}
+	else
+	{
 		optref.mouse.y_sensitivity = 0.5;
 	}
 	// video colors
-	if( CheckAttribute(&InterfaceStates,"video.contrast") ) {
+	if (CheckAttribute(&InterfaceStates, "video.contrast"))
+	{
 		optref.video.contrast = InterfaceStates.video.contrast;
-	} else {
+	}
+	else
+	{
 		optref.video.contrast = 1.0;
 	}
-	if( CheckAttribute(&InterfaceStates,"video.gamma") ) {
+	if (CheckAttribute(&InterfaceStates, "video.gamma"))
+	{
 		optref.video.gamma = InterfaceStates.video.gamma;
-	} else {
+	}
+	else
+	{
 		optref.video.gamma = 1.0;
 	}
-	if( CheckAttribute(&InterfaceStates,"video.brightness") ) {
+	if (CheckAttribute(&InterfaceStates, "video.brightness"))
+	{
 		optref.video.brightness = InterfaceStates.video.brightness;
-	} else {
+	}
+	else
+	{
 		optref.video.brightness = 0.0;
 	}
 
@@ -228,16 +298,19 @@ void GetRealOptions(ref optref)
 	SeaParametrs.LodScale = LodScale;
 
 	// always run
-	if( CheckAttribute(&InterfaceStates,"alwaysrun") ) {
+	if (CheckAttribute(&InterfaceStates, "alwaysrun"))
+	{
 		optref.alwaysrun = InterfaceStates.alwaysrun;
-	} else {
+	}
+	else
+	{
 		optref.alwaysrun = false;
 	}
 }
 
 void SetCurentOptions(ref optref)
 {
-	SendMessage(&sound,"lfff", MSG_SOUND_SET_MASTER_VOLUME, stf(optref.volume.sound),	stf(optref.volume.music),	stf(optref.volume.dialog));
+	SendMessage(&sound, "lfff", MSG_SOUND_SET_MASTER_VOLUME, stf(optref.volume.sound), stf(optref.volume.music), stf(optref.volume.dialog));
 
 	/*bArcadeSails = sti(optref.arcademode.bArcadeSails);
 	bArcadeShipSpeed = sti(optref.arcademode.bArcadeShipSpeed);
@@ -257,161 +330,234 @@ void SetCurentOptions(ref optref)
 	SeaMaxWaveDistance
 	*/
 
-	if( CheckAttribute(optref,"cameramode.ShowBattleMode") ) {
+	if (CheckAttribute(optref, "cameramode.ShowBattleMode"))
+	{
 		InterfaceStates.ShowBattleMode = optref.cameramode.ShowBattleMode;
-	} else {
+	}
+	else
+	{
 		InterfaceStates.ShowBattleMode = false;
 	}
 
-	if( CheckAttribute(optref,"cameramode.EnabledAutoSaveMode") ) {
+	if (CheckAttribute(optref, "cameramode.EnabledAutoSaveMode"))
+	{
 		InterfaceStates.EnabledAutoSaveMode = optref.cameramode.EnabledAutoSaveMode;
-	} else {
+	}
+	else
+	{
 		InterfaceStates.EnabledAutoSaveMode = true;
 	}
 
-	if( CheckAttribute(optref,"cameramode.EnabledQuestsMarks") ) {
+	if (CheckAttribute(optref, "cameramode.EnabledQuestsMarks"))
+	{
 		InterfaceStates.EnabledQuestsMarks = optref.cameramode.EnabledQuestsMarks;
-	} else {
+	}
+	else
+	{
 		InterfaceStates.EnabledQuestsMarks = true;
 	}
 
-	if( CheckAttribute(optref,"cameramode.EnabledFXMarks") ) {
+	if (CheckAttribute(optref, "cameramode.EnabledFXMarks"))
+	{
 		InterfaceStates.EnabledFXMarks = optref.cameramode.EnabledFXMarks;
-	} else {
+	}
+	else
+	{
 		InterfaceStates.EnabledFXMarks = true;
 	}
 
-	if( CheckAttribute(optref,"cameramode.EnabledCMControls") ) {
+	if (CheckAttribute(optref, "cameramode.EnabledCMControls"))
+	{
 		InterfaceStates.EnabledCMControls = optref.cameramode.EnabledCMControls;
-	} else {
+	}
+	else
+	{
 		InterfaceStates.EnabledCMControls = false;
 	}
 
-	if( CheckAttribute(optref,"cameramode.EnabledShipMarks") ) {
+	if (CheckAttribute(optref, "cameramode.EnabledShipMarks"))
+	{
 		InterfaceStates.EnabledShipMarks = optref.cameramode.EnabledShipMarks;
-	} else {
+	}
+	else
+	{
 		InterfaceStates.EnabledShipMarks = true;
 	}
 
-	if( CheckAttribute(optref,"cameramode.SimpleSeaMode") ) {
+	if (CheckAttribute(optref, "cameramode.SimpleSeaMode"))
+	{
 		InterfaceStates.SimpleSea = optref.cameramode.SimpleSeaMode;
-	} else {
+	}
+	else
+	{
 		InterfaceStates.SimpleSea = false;
 	}
 
-	if( CheckAttribute(optref,"cameramode.SpyglassTextures") ) {
+	if (CheckAttribute(optref, "cameramode.SpyglassTextures"))
+	{
 		InterfaceStates.SpyglassTextures = optref.cameramode.SpyglassTextures;
-	} else {
+	}
+	else
+	{
 		InterfaceStates.SpyglassTextures = false;
 	}
 
-	if( CheckAttribute(optref,"cameramode.HUDStyle") ) {
+	if (CheckAttribute(optref, "cameramode.HUDStyle"))
+	{
 		InterfaceStates.HUDStyle = optref.cameramode.HUDStyle;
-	} else {
+	}
+	else
+	{
 		InterfaceStates.HUDStyle = false;
 	}
 
-	if( CheckAttribute(optref,"cameramode.VISUAL_CIRASS") ) {
+	if (CheckAttribute(optref, "cameramode.VISUAL_CIRASS"))
+	{
 		InterfaceStates.VISUAL_CIRASS = optref.cameramode.VISUAL_CIRASS;
-	} else {
+	}
+	else
+	{
 		InterfaceStates.VISUAL_CIRASS = 0;
 	}
 
-	if( CheckAttribute(optref,"cameramode.CannonsHUD") ) {
+	if (CheckAttribute(optref, "cameramode.CannonsHUD"))
+	{
 		InterfaceStates.CannonsHUD = optref.cameramode.CannonsHUD;
-	} else {
+	}
+	else
+	{
 		InterfaceStates.CannonsHUD = false;
 	}
 
-	if( CheckAttribute(optref,"cameramode.DeadBoxText") ) {
+	if (CheckAttribute(optref, "cameramode.DeadBoxText"))
+	{
 		InterfaceStates.DeadBoxText = optref.cameramode.DeadBoxText;
-	} else {
+	}
+	else
+	{
 		InterfaceStates.DeadBoxText = false;
 	}
 
-	if( CheckAttribute(optref,"cameramode.AltFont") ) {
+	if (CheckAttribute(optref, "cameramode.AltFont"))
+	{
 		InterfaceStates.AltFont = optref.cameramode.AltFont;
-	} else {
+	}
+	else
+	{
 		InterfaceStates.AltFont = 0;
 	}
-	if( CheckAttribute(optref,"cameramode.NoInt") ) {
+	if (CheckAttribute(optref, "cameramode.NoInt"))
+	{
 		InterfaceStates.NoInt = optref.cameramode.NoInt;
-	} else {
+	}
+	else
+	{
 		InterfaceStates.NoInt = false;
 	}
-	if( CheckAttribute(optref,"cameramode.IslandLoader") ) {
+	if (CheckAttribute(optref, "cameramode.IslandLoader"))
+	{
 		InterfaceStates.IslandLoader = optref.cameramode.IslandLoader;
-	} else {
+	}
+	else
+	{
 		InterfaceStates.IslandLoader = false;
 	}
 
-	if( CheckAttribute(optref,"cameramode.AltIntIcons") ) {
+	if (CheckAttribute(optref, "cameramode.AltIntIcons"))
+	{
 		InterfaceStates.AltIntIcons = optref.cameramode.AltIntIcons;
-	} else {
+	}
+	else
+	{
 		InterfaceStates.AltIntIcons = false;
 	}
 
-
 	// mouse
-	if( CheckAttribute(optref,"cameramode.InvertCameras") ) {
+	if (CheckAttribute(optref, "cameramode.InvertCameras"))
+	{
 		InterfaceStates.InvertCameras = optref.cameramode.InvertCameras;
-	} else {
+	}
+	else
+	{
 		InterfaceStates.InvertCameras = false;
 	}
-	if( CheckAttribute(optref,"mouse.x_sensitivity") ) {
+	if (CheckAttribute(optref, "mouse.x_sensitivity"))
+	{
 		InterfaceStates.mouse.x_sens = optref.mouse.x_sensitivity;
-	} else {
+	}
+	else
+	{
 		InterfaceStates.mouse.x_sens = 0.5;
 	}
-	if( CheckAttribute(optref,"mouse.y_sensitivity") ) {
+	if (CheckAttribute(optref, "mouse.y_sensitivity"))
+	{
 		InterfaceStates.mouse.y_sens = optref.mouse.y_sensitivity;
-	} else {
+	}
+	else
+	{
 		InterfaceStates.mouse.y_sens = 0.5;
 	}
 	// video colors
-	if( CheckAttribute(optref,"video.contrast") ) {
+	if (CheckAttribute(optref, "video.contrast"))
+	{
 		InterfaceStates.video.contrast = optref.video.contrast;
-	} else {
+	}
+	else
+	{
 		InterfaceStates.video.contrast = 1.0;
 	}
-	if( CheckAttribute(optref,"video.gamma") ) {
+	if (CheckAttribute(optref, "video.gamma"))
+	{
 		InterfaceStates.video.gamma = optref.video.gamma;
-	} else {
+	}
+	else
+	{
 		InterfaceStates.video.gamma = 1.0;
 	}
-	if( CheckAttribute(optref,"video.brightness") ) {
+	if (CheckAttribute(optref, "video.brightness"))
+	{
 		InterfaceStates.video.brightness = optref.video.brightness;
-	} else {
+	}
+	else
+	{
 		InterfaceStates.video.brightness = 0.0;
 	}
 
-	if( CheckAttribute(optref,"video.grassquantity") ) {
+	if (CheckAttribute(optref, "video.grassquantity"))
+	{
 		iGrassQuality = sti(optref.video.grassquantity);
 	}
-	if( CheckAttribute(optref,"seadetails") ) {
+	if (CheckAttribute(optref, "seadetails"))
+	{
 		InterfaceStates.SeaDetails = stf(optref.seadetails);
 	}
-    //#20171223-01 Camera perspective option
-	if( CheckAttribute(optref,"perspdetails") ) {
+	//#20171223-01 Camera perspective option
+	if (CheckAttribute(optref, "perspdetails"))
+	{
 		InterfaceStates.PerspDetails = stf(optref.perspdetails);
 	}
 	//#20190815-01
-	if( CheckAttribute(optref,"persp2details") ) {
+	if (CheckAttribute(optref, "persp2details"))
+	{
 		InterfaceStates.Persp2Details = stf(optref.persp2details);
 	}
-	if( CheckAttribute(optref,"raddetails") ) {
+	if (CheckAttribute(optref, "raddetails"))
+	{
 		InterfaceStates.RadDetails = stf(optref.raddetails);
 	}
-	XI_SetColorCorrection(stf(InterfaceStates.video.contrast),stf(InterfaceStates.video.gamma),stf(InterfaceStates.video.brightness));
+	XI_SetColorCorrection(stf(InterfaceStates.video.contrast), stf(InterfaceStates.video.gamma), stf(InterfaceStates.video.brightness));
 
 	aref arControls;
-	makearef(arControls,optref.controls);
+	makearef(arControls, optref.controls);
 	RestoreKeysFromOptions(arControls);
 
 	// always run
-	if( CheckAttribute(&optref,"alwaysrun") ) {
+	if (CheckAttribute(&optref, "alwaysrun"))
+	{
 		InterfaceStates.alwaysrun = optref.alwaysrun;
-	} else {
+	}
+	else
+	{
 		InterfaceStates.alwaysrun = false;
 	}
 
@@ -422,8 +568,9 @@ void SetCurentOptions(ref optref)
 void ReadSavedOptions(ref gopt)
 {
 	string sFileName = "options";
-	if( CheckAttribute(&PlayerProfile,"name") && PlayerProfile.name!="" ) {
-		sFileName = "SAVE" + PlayerProfile.name+"\options\options";
+	if (CheckAttribute(&PlayerProfile, "name") && PlayerProfile.name != "")
+	{
+		sFileName = "SAVE\" + PlayerProfile.name+"\options\options ";
 	}
 	SendMessage(&GameInterface, "lsa", MSG_INTERFACE_LOADOPTIONS, sFileName, gopt);
 }
@@ -431,34 +578,35 @@ void ReadSavedOptions(ref gopt)
 void SaveSavedOptions(ref gopt)
 {
 	string sFileName = "options";
-	if( CheckAttribute(&PlayerProfile,"name") && PlayerProfile.name!="" ) {
-		sFileName = "SAVE" + PlayerProfile.name+"\options\options";
+	if (CheckAttribute(&PlayerProfile, "name") && PlayerProfile.name != "")
+	{
+		sFileName = "SAVE\" + PlayerProfile.name + "\options\options ";
 	}
 	SendMessage(&GameInterface, "lsa", MSG_INTERFACE_SAVEOPTIONS, sFileName, gopt);
 }
 
 void GetControlsOptions(ref optref)
 {
-	int ng,qg, nc,qc;
+	int ng, qg, nc, qc;
 	aref arIn, arOut, arGrp, arCntrl;
 	string grName, cntrlName;
 
-	makearef(arOut,objControlsState.keygroups);
+	makearef(arOut, objControlsState.keygroups);
 	optref.controls = true;
-	makearef(arIn,optref.controls);
+	makearef(arIn, optref.controls);
 
 	qg = GetAttributesNum(arOut);
-	for(ng=0; ng<qg; ng++)
+	for (ng = 0; ng < qg; ng++)
 	{
-		arGrp = GetAttributeN(arOut,ng);
+		arGrp = GetAttributeN(arOut, ng);
 		grName = GetAttributeName(arGrp);
-		if( CheckAttribute(&objControlsState,"grouplist."+grName) && sti(objControlsState.grouplist.(grName))==true )
+		if (CheckAttribute(&objControlsState, "grouplist." + grName) && sti(objControlsState.grouplist.(grName)) == true)
 		{
 			qc = GetAttributesNum(arGrp);
-			for(nc=0; nc<qc; nc++)
+			for (nc = 0; nc < qc; nc++)
 			{
-				arCntrl = GetAttributeN(arGrp,nc);
-				if( CheckAttribute(arCntrl,"remapping") && sti(arCntrl.remapping)==true )
+				arCntrl = GetAttributeN(arGrp, nc);
+				if (CheckAttribute(arCntrl, "remapping") && sti(arCntrl.remapping) == true)
 				{
 					cntrlName = GetAttributeName(arCntrl);
 					arIn.(grName).(cntrlName) = GetAttributeValue(arCntrl);
