ada6a79d45171d7a03e80dc014e63bbb452f2e1e
Gregg <VVVHUNTER@yandex.ru>
2022-07-27 00:50:41 +0600

    Fixed debugger visual
    -Fixed debugger visual (was bugged)

--- Program/Quests/quests.c
+++ Program/Quests/quests.c
@@ -1933,7 +1933,7 @@ void CalculateCheatsInfo() // Для статистики по читам. Ис�
 	makearef(arAllCheats, PChar.Statistic.Cheats);
 	int iAllCheats = GetAttributesNum(arAllCheats);
 	int iAllCheatsCount = 0;
-	/*
+	
 
 	SetFormatedText("INFO_TEXT2", "Игра начата на версии:\n" + pchar.versionnumber + "\nИспользовавшиеся читы:");
 
@@ -1954,5 +1954,5 @@ void CalculateCheatsInfo() // Для статистики по читам. Ис�
 	AddLineToFormatedText("INFO_TEXT2", XI_ConvertString("Total") + " " + iAllCheatsCount + " раз.");
 
 	if(iAllCheatsCount == 0) // Исли 0, значит читов не юзали
-		SetFormatedText("INFO_TEXT2", "Игра начата на версии:\n" + pchar.versionnumber + "\nЧитов не использовалось!");*/
+		SetFormatedText("INFO_TEXT2", "Игра начата на версии:\n" + pchar.versionnumber + "\nЧитов не использовалось!");
 }
